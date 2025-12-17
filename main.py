8bf3c1840584de1934a2d966d7639465fea59608
sai <sainitinyadav.mudike@gmail.com>
2025-12-16 21:30:46 -0500

    add nato logic

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pandas as pd
-data_frame=pd.read_csv("nato_phonetic_alphabet.csv")
-print(data_frame)
 
-nato={row.letter:row.code for (index,row) in data_frame.iterrows()}
+
+
+
+data_frame=pd.read_csv("nato_phonetic_alphabet.csv")
+nato_data={row.letter:row.code for (index,row) in data_frame.iterrows()}
+
+
+user_input=input("Enter a word: ").upper()
+
+nato_words=[nato_data[letter] if letter in nato_data else None for letter in user_input]
+
+print(nato_words)
+
